4946a9441fdc483988738cf0e551125c4fc2a7f3
Xabier Gardeazabal <xgardeazabal@gmail.com>
2015-02-26 18:37:03 +0100

    Fixed preferences: the list-selection elements now show the current selection in the summary

--- src/org/egokituz/arduino2android/activities/SettingsActivity.java
+++ src/org/egokituz/arduino2android/activities/SettingsActivity.java
@@ -15,9 +15,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
 import android.preference.PreferenceManager;
 
 /**
@@ -39,24 +42,77 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        initSummary(getPreferenceScreen());
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// Set up a listener whenever a key changes
+		getPreferenceScreen().getSharedPreferences()
+		.registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// Unregister the listener whenever a key changes
+		getPreferenceScreen().getSharedPreferences()
+		.unregisterOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-	    Preference pref = findPreference(key);
 
-	    //TODO: set summary for the preferences
-	    if (pref instanceof ListPreference) {
-	        ListPreference listPref = (ListPreference) pref;
-	        pref.setSummary(listPref.getEntry());
-	    }
-	
-	    /*
+
+		updatePrefSummary(findPreference(key));
+
+		//TODO: set summary for the preferences
+		Preference pref = findPreference(key);
+		//		if (pref instanceof ListPreference) {
+		//			ListPreference listPref = (ListPreference) pref;
+		//			pref.setSummary(listPref.getEntry());
+		//		}
+
+		/*
 		if (key.equals(PREF_DISCOVERY_PLAN)) {
 			Preference connectionPref = findPreference(key);
 			// Set summary to be the user-description for the selected value
 			connectionPref.setSummary(sharedPreferences.getString(key, ""));
 		}*/
+	}
+
+	private void initSummary(Preference p) {
+		if (p instanceof PreferenceGroup) {
+			PreferenceGroup pGrp = (PreferenceGroup) p;
+			for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+				initSummary(pGrp.getPreference(i));
+			}
+		} else {
+			updatePrefSummary(p);
+		}
+	}
+
+	private void updatePrefSummary(Preference p) {
+		if (p instanceof ListPreference) {
+			ListPreference listPref = (ListPreference) p;
+			p.setSummary(listPref.getEntry());
+		}
+		if (p instanceof EditTextPreference) {
+			EditTextPreference editTextPref = (EditTextPreference) p;
+			if (p.getTitle().toString().contains("assword"))
+			{
+				p.setSummary("******");
+			} else {
+				p.setSummary(editTextPref.getText());
+			}
+		}
+		if (p instanceof MultiSelectListPreference) {
+			EditTextPreference editTextPref = (EditTextPreference) p;
+			p.setSummary(editTextPref.getText());
+		}
 	}
 
 	public static Map<String, Integer> getCurrentPreferences(Context context){
